b448d9a002ca5f164b10c19491c27b7a356f150d
mhenri12 <miguelhenrico01@gmail.com>
2026-08-31 09:41:36 -0300

    Update tentativa.py

--- aula4/tentativa.py
+++ aula4/tentativa.py
@@ -1,1 +1,26 @@
+class Pilha
+    def __init__(self):
+    self.pilha = []
 
+    def empilhar(self, acao):
+        self.pilha.append(acao)
+    
+    def desfazer(self):
+        if len(self.pilha) > 0:
+            return self.pilha.pop()
+        else:
+            return "nada"
+    
+    def mostrar(self):
+        print(self.pilha)
+    
+    pilha = Pilha ()
+
+    pilha.empilhar("1")
+    pilha.empilhar("2")
+    pilha.empilhar("3")
+
+    pilha.mostrar()
+    print("desfeito", pilha.desfazer())
+
+    pilha.mostrar()
