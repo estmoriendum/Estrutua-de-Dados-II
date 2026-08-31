72cd74afacb967c3bf2f8f1ff88a9f88ebedc6b4
mhenri12 <miguelhenrico01@gmail.com>
2026-08-31 09:49:28 -0300

    Update tentativa2.py

--- aula4/tentativa2.py
+++ aula4/tentativa2.py
@@ -1,1 +1,33 @@
+class Fila:
+    def __init__(self):
+    self.fila = []
+
+    def enrar(self, documneto):
+        self.fila.append(documento)
+    
+    def desfazer(self):
+        if len(self.fila) > 0:
+            documento = self.fila.pop()
+            print("imprimido",documento)
+        else:
+            return "nada"
+    
+    def mostrar(self):
+        print(self.fila)
+
+spooler = Fila()
+
+spooler.entrar("1")
+spooler.entrar("2")
+spooler.entrar("3")
+spooler.entrar("4")
+
+spooler.mostrar()
+
+spooler.imprimir()
+spooler.imprimir()
+
+spooler.mostrar()
+
+
 
